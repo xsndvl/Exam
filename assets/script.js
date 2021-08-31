45f653416fc897e813ee80354ea0a4782628dd10
Joswag Sandoval <sergiojr100@outlook.com>
2021-08-30 20:49:42 -0500

    Did best I could to make code work

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,63 @@
-// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Sergio Sandoval" 
 
-// We'll use these variables to track the counts of each cookie type
+
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
-// Code to update name display 
+const gbfinal = document.getElementById("qty-gb")
+const ccfinal = document.getElementById("qty-cc")
+const sugarfinal = document.getElementById("qty-sugar")
+const totalfinal = document.getElementById("qty-total")
+
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
+//gb
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
+    gb ++
     console.log('Ginger bread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+})
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if (gb > 0 ){
+        gb --
+        console.log('Ginger bread - button was clicked!')
+    }
+    
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+//cc
+document.getElementById("add-cc").addEventListener("click", function(){
+    cc ++
+    console.log("Chocolate Chip + button was clicked!")
+})
+document.getElementById("minus-cc").addEventListener("click",function(){
+    if (cc > 0){
+        cc --
+        console.log("Chocolate Chip - button was clicked!")
+    }
+    
+})
+
+//sugar
+document.getElementById("add-sugar").addEventListener('click',function(){
+    sugar ++
+    console.log("Sugar Sprinkle + button was clicked!")
+})
+document.getElementById("minus-sugar").addEventListener('click',function(){
+    if (sugar>0){
+        sugar--
+        console.log("Sugar Sprinkle - button was clicked!")
+    }
+})
+
+//total
+let total = sugar + cc + gb
+
+gbfinal.textContent = gb
+ccfinal.textContent = cc
+sugarfinal.textContent = sugar
+totalfinal.textContent = total
+
+
